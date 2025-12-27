b930b6cd60d6be3bbc4b6543b48a88dd8a5dffb8
Amadou DEME <demea2682@gmail.com>
2025-12-27 05:17:42 +0100

    Update Teams

--- src/Components/About/AboutTeam.tsx
+++ src/Components/About/AboutTeam.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 const team = [
   {
     name: "Amadou Deme",
-    role: "Fondateur & COE",
+    role: "COE & Mentor en Développement d'application",
     image: "images/Teams/Fundater.png",
     linkedin: "#",
     email: "fondateur@janghubtech.com",
